74f14c41f69da88db7e3bdf54221e5871757b67c
art0007i <art0007i@gmail.com>
2025-09-07 19:11:08 +0200

    Delete ResoniteModLoader/Settings directory

--- ResoniteModLoader/Settings/ModSettings.cs
+++
@@ -1,9 +0,0 @@
-using FrooxEngine;
-
-namespace ResoniteModLoader;
-
-[AutoRegisterSetting]
-[SettingCategory("ResoniteModLoader")]
-public sealed class ModSettings : SettingComponent<ModSettings> {
-	public override bool UserspaceOnly => true;
-}
