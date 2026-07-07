eb9d3ea927568336a58830568e1c7b51e46c6f50
art0007i <art0007i@gmail.com>
2026-07-07 19:17:08 +0200

    remove some unneeded stuff

--- ResoniteModLoader/Properties/AssemblyInfo.cs
+++ ResoniteModLoader/Properties/AssemblyInfo.cs
@@ -4,5 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
+// Prevent FrooxEngine.Weaver from modifying this assembly, as it doesn't need anything done to it
+// This keeps Weaver from overwriting AssemblyVersionAttribute
+[module: Description("FROOXENGINE_WEAVED")]
+
 //Mark as DataModelAssembly for the Plugin loading system to load this assembly
-[assembly: DataModelAssembly(DataModelAssemblyType.UserspaceCore)]
+[assembly: DataModelAssembly(DataModelAssemblyType.Optional)]
